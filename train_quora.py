42da53415e2e21bba9a2b288c5b230dd62d334ab
lihongqiang <415200973@qq.com>
2017-05-26 01:19:35 +0800

    finish TextCNN and TextRNN

--- train_quora.py
+++ train_quora.py
@@ -2,13 +2,26 @@
 
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 import os
 import time
-import datetime
 import data_helpers
-from text_cnn import TextCNN
-from tensorflow.contrib import learn
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 
+#from tensorflow.contrib.keras.api.keras.models import Model
+from keras.models import Model
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+#from keras.layers.embeddings import Embedding
+from keras.layers import Embedding, Input, Dense, Dropout, Conv1D, MaxPooling1D, Flatten, concatenate, LSTM
+from keras.layers.normalization import BatchNormalization
+from keras import regularizers
+from keras.backend import expand_dims, reshape
+from keras.layers.merge import concatenate
+
+
+from gensim.models import KeyedVectors
+#from keras.engine import topology
 # set default encoding
 # -*- coding:utf-8 -*-
 import sys
@@ -19,25 +32,31 @@ sys.setdefaultencoding('utf-8')
 # ==================================================
 
 # Data loading params
-tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training data to use for validation")
+tf.flags.DEFINE_float("valid_sample_percentage", 0.1, "Percentage of the training data to use for validation")
 tf.flags.DEFINE_string("positive_data_file", "./data/rt-polaritydata/rt-polarity.pos", "Data source for the positive data.")
 tf.flags.DEFINE_string("negative_data_file", "./data/rt-polaritydata/rt-polarity.neg", "Data source for the negative data.")
 tf.flags.DEFINE_string("quora_train_file", "./data/quora/train.csv", "Quora train data.")
-
+tf.flags.DEFINE_string("test_data_file", "./data/quora/test.csv", "Test Data file")
+tf.flags.DEFINE_string("embedding_file", "./data/quora/GoogleNews-vectors-negative300.bin", "Embedding Data file")
 
 # Model Hyperparameters
-tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
+tf.flags.DEFINE_integer("embedding_dim", 300, "Dimensionality of character embedding (default: 128)")
 tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
 tf.flags.DEFINE_float("l2_reg_lambda", 1, "L2 regularization lambda (default: 0.0)")
+tf.flags.DEFINE_integer("max_nb_words", 200000, "number of words taking into count. 125642")
+tf.flags.DEFINE_integer("max_sequence_length", 150, "number of words in sequence taking into count.")
 
 # Training parameters
-tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_integer("num_epochs", 100, "Number of training epochs (default: 200)")
-tf.flags.DEFINE_integer("evaluate_every", 1000, "Evaluate model on dev set after this many steps (default: 100)")
-tf.flags.DEFINE_integer("checkpoint_every", 1000, "Save model after this many steps (default: 100)")
+tf.flags.DEFINE_integer("batch_size", 2048, "Batch Size (default: 64)")
+tf.flags.DEFINE_integer("num_epochs", 200, "Number of training epochs (default: 200)")
+tf.flags.DEFINE_integer("evaluate_every", 10, "Evaluate model on dev set after this many steps (default: 100)")
+tf.flags.DEFINE_integer("output_every", 1, "Output model on dev set after this many steps (default: 100)")
+tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
 tf.flags.DEFINE_integer("num_checkpoints", 5, "Number of checkpoints to store (default: 5)")
+tf.flags.DEFINE_bool("re_weight", True, "whether to re-weight classes to fit the 17.5% share in test set")
+
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
 tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
@@ -53,158 +72,205 @@ print("")
 # Data Preparation
 # ==================================================
 
+####################################################
 # Load data
+####################################################
 print("Loading data...")
 x_text_q1, x_text_q2, y = data_helpers.load_data_and_labels_from_quora(FLAGS.quora_train_file)
+test_id, test_text_q1, test_text_q2 = data_helpers.load_data_from_quora(FLAGS.test_data_file)
 
+####################################################
 # Build vocabulary
-max_document_length_q1 = max([len(x.split(" ")) for x in x_text_q1])
-max_document_length_q2 = max([len(x.split(" ")) for x in x_text_q2])
-max_document_length = max([max_document_length_q1, max_document_length_q2])
+####################################################
+print("Building vocabulary...")
+tokenizer = Tokenizer(num_words=FLAGS.max_nb_words)
+tokenizer.fit_on_texts(x_text_q1 + x_text_q2 + test_text_q1 + test_text_q2)
 
-vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
-x_q1 = np.array(list(vocab_processor.fit_transform(x_text_q1)))
-x_q2 = np.array(list(vocab_processor.fit_transform(x_text_q2)))
+sequences_1 = tokenizer.texts_to_sequences(x_text_q1)
+sequences_2 = tokenizer.texts_to_sequences(x_text_q2)
+test_sequences_1 = tokenizer.texts_to_sequences(test_text_q1)
+test_sequences_2 = tokenizer.texts_to_sequences(test_text_q2)
 
-# Randomly shuffle data
-np.random.seed(10)
-shuffle_indices = np.random.permutation(np.arange(len(y)))
+word_index = tokenizer.word_index
+print('Found %s unique tokens' % len(word_index))
 
-x_q1_shuffled = x_q1[shuffle_indices]
-x_q2_shuffled = x_q2[shuffle_indices]
+data_1 = pad_sequences(sequences_1, maxlen=FLAGS.max_sequence_length)
+data_2 = pad_sequences(sequences_2, maxlen=FLAGS.max_sequence_length)
 
-print len(shuffle_indices)
-print len(y)
-y_shuffled = y[shuffle_indices]
+print('Shape of data tensor:', data_1.shape)
+print('data type:', data_1.dtype)
+print('Shape of label tensor:', y.shape)
+print('data type:', y.dtype)
 
-# Split train/test set
-# TODO: This is very crude, should use cross-validation
-dev_sample_index = -1 * int(FLAGS.dev_sample_percentage * float(len(y)))
-x_train_q1, x_dev_q1 = x_q1_shuffled[:dev_sample_index], x_q1_shuffled[dev_sample_index:]
-x_train_q2, x_dev_q2 = x_q2_shuffled[:dev_sample_index], x_q2_shuffled[dev_sample_index:]
-y_train, y_dev = y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
-print("Vocabulary Size: {:d}".format(len(vocab_processor.vocabulary_)))
-print("Train/Dev split: {:d}/{:d}".format(len(y_train), len(y_dev)))
+test_data_1 = pad_sequences(test_sequences_1, maxlen=FLAGS.max_sequence_length)
+test_data_2 = pad_sequences(test_sequences_2, maxlen=FLAGS.max_sequence_length)
 
+#####################################################
+## index word vectors
+#####################################################
+print('Indexing word vectors')
+word2vec = KeyedVectors.load_word2vec_format(FLAGS.embedding_file, binary=True)
+print('Found %s word vectors of word2vec' % len(word2vec.vocab))
 
+#####################################################
+## prepare embeddings
+#####################################################
+print('Preparing embedding matrix')
+nb_words = min(FLAGS.max_nb_words, len(word_index))+1
+embedding_matrix = np.zeros((nb_words, FLAGS.embedding_dim))
+for word, i in word_index.items():
+    if word in word2vec.vocab:
+        embedding_matrix[i] = word2vec.word_vec(word)
+print('Null word embeddings: %d' % np.sum(np.sum(embedding_matrix, axis=1) == 0))
+
+## sample train/validation data
+#np.random.seed(1234)
+perm = np.random.permutation(len(y))
+idx_train = perm[:int(len(y)*(1-FLAGS.valid_sample_percentage))]
+idx_val = perm[int(len(y)*(1-FLAGS.valid_sample_percentage)):]
+
+data_1_train = np.vstack((data_1[idx_train], data_2[idx_train]))
+data_2_train = np.vstack((data_2[idx_train], data_1[idx_train]))
+labels_train = np.concatenate((y[idx_train], y[idx_train]))
+
+data_1_val = np.vstack((data_1[idx_val], data_2[idx_val]))
+data_2_val = np.vstack((data_2[idx_val], data_1[idx_val]))
+labels_val = np.concatenate((y[idx_val], y[idx_val]))
+
+print('Shape of train data tensor:', data_1_train.shape)
+print('Shape of train label tensor:', labels_train.shape)
+
+print('Shape of valid data tensor:', data_1_val.shape)
+print('Shape of valid label tensor:', labels_val.shape)   
+
+########################################
+## add class weight
+########################################
+if FLAGS.re_weight:
+    class_weight = {0: 1.309028344, 1: 0.472001959}
+else:
+    class_weight = None
+
+weight_val = np.ones(len(labels_val))
+if FLAGS.re_weight:
+    weight_val *= 0.472001959
+    weight_val[labels_val==0] = 1.309028344
+       
 # Training
-# ==================================================
+# =================================================
+###################################################   
+## TextCNN
+###################################################
+embedding_size=FLAGS.embedding_dim
+filter_sizes=list(map(int, FLAGS.filter_sizes.split(",")))
+num_filters=FLAGS.num_filters
+#num_filters_total = num_filters * len(filter_sizes) * 2
 
-with tf.Graph().as_default():
-    session_conf = tf.ConfigProto(
-      allow_soft_placement=FLAGS.allow_soft_placement,
-      log_device_placement=FLAGS.log_device_placement)
-    sess = tf.Session(config=session_conf)
-    with sess.as_default():
-        cnn = TextCNN(
-            sequence_length=x_train_q1.shape[1],
-            num_classes=y_train.shape[1],
-            vocab_size=len(vocab_processor.vocabulary_),
-            embedding_size=FLAGS.embedding_dim,
-            filter_sizes=list(map(int, FLAGS.filter_sizes.split(","))),
-            num_filters=FLAGS.num_filters,
-            l2_reg_lambda=FLAGS.l2_reg_lambda)
+embedding_layer = Embedding(input_dim=nb_words,
+    output_dim=embedding_size,
+    weights=[embedding_matrix],
+    input_length=FLAGS.max_sequence_length,
+    trainable=False)
 
-        # Define Training procedure
-        global_step = tf.Variable(0, name="global_step", trainable=False)
-        optimizer = tf.train.AdamOptimizer(1e-3)
-        grads_and_vars = optimizer.compute_gradients(cnn.loss)
-        train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+merging_list = []
+sequence_input = Input(shape=(FLAGS.max_sequence_length,), dtype='int32')
+for filter_size in filter_sizes:
+    embedded_chars_q = embedding_layer(sequence_input)
+    conv = Conv1D(filters=num_filters, kernel_size=filter_size, activation='relu')(embedded_chars_q)
+    pooled = MaxPooling1D(pool_size=FLAGS.max_sequence_length - filter_size + 1)(conv)
+    flat = Flatten()(pooled)
+    merging_list.append(flat)
+merged_seq = concatenate(merging_list)
 
-        # Keep track of gradient values and sparsity (optional)
-        grad_summaries = []
-        for g, v in grads_and_vars:
-            if g is not None:
-                grad_hist_summary = tf.summary.histogram("{}/grad/hist".format(v.name), g)
-                sparsity_summary = tf.summary.scalar("{}/grad/sparsity".format(v.name), tf.nn.zero_fraction(g))
-                grad_summaries.append(grad_hist_summary)
-                grad_summaries.append(sparsity_summary)
-        grad_summaries_merged = tf.summary.merge(grad_summaries)
+question_model = Model(sequence_input, merged_seq)
+sequence_1_input = Input(shape=(FLAGS.max_sequence_length,), dtype='int32')
+sequence_2_input = Input(shape=(FLAGS.max_sequence_length,), dtype='int32')
 
-        # Output directory for models and summaries
-        timestamp = str(int(time.time()))
-        out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
-        print("Writing to {}\n".format(out_dir))
+q1 = question_model(sequence_1_input)
+q2 = question_model(sequence_2_input)
 
-        # Summaries for loss and accuracy
-        loss_summary = tf.summary.scalar("loss", cnn.loss)
-        acc_summary = tf.summary.scalar("accuracy", cnn.accuracy)
+merged = concatenate([q1, q2])
+merged = Dropout(FLAGS.dropout_keep_prob)(merged)
+merged = BatchNormalization()(merged)
 
-        # Train Summaries
-        train_summary_op = tf.summary.merge([loss_summary, acc_summary, grad_summaries_merged])
-        train_summary_dir = os.path.join(out_dir, "summaries", "train")
-        train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
+#merged = Dense(10, activation='relu')(merged)
+#merged = Dropout(FLAGS.dropout_keep_prob)(merged)
+#merged = BatchNormalization()(merged)
 
-        # Dev summaries
-        dev_summary_op = tf.summary.merge([loss_summary, acc_summary])
-        dev_summary_dir = os.path.join(out_dir, "summaries", "dev")
-        dev_summary_writer = tf.summary.FileWriter(dev_summary_dir, sess.graph)
+preds = Dense(1, activation='sigmoid')(merged)
 
-        # Checkpoint directory. Tensorflow assumes this directory already exists so we need to create it
-        checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
-        checkpoint_prefix = os.path.join(checkpoint_dir, "model")
-        if not os.path.exists(checkpoint_dir):
-            os.makedirs(checkpoint_dir)
-        saver = tf.train.Saver(tf.global_variables(), max_to_keep=FLAGS.num_checkpoints)
+########################################
+## TextRNN
+########################################
 
-        # Write vocabulary
-        vocab_processor.save(os.path.join(out_dir, "vocab"))
+#EMBEDDING_DIM = 300
+#MAX_SEQUENCE_LENGTH = FLAGS.max_sequence_length
+#rate_drop_dense = 0.5
+#rate_drop_lstm = 0.5
+#num_lstm = 200
+#num_dense= 100
+#embedding_layer = Embedding(nb_words,
+#        EMBEDDING_DIM,
+#        weights=[embedding_matrix],
+#        input_length=MAX_SEQUENCE_LENGTH,
+#        trainable=False)
+#lstm_layer = LSTM(num_lstm, dropout=rate_drop_lstm, recurrent_dropout=rate_drop_lstm)
+#
+#sequence_1_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+#embedded_sequences_1 = embedding_layer(sequence_1_input)
+#x1 = lstm_layer(embedded_sequences_1)
+#
+#sequence_2_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+#embedded_sequences_2 = embedding_layer(sequence_2_input)
+#y1 = lstm_layer(embedded_sequences_2)
+#
+#merged = concatenate([x1, y1])
+#merged = Dropout(rate_drop_dense)(merged)
+#merged = BatchNormalization()(merged)
+#
+#merged = Dense(num_dense, activation='relu')(merged)
+#merged = Dropout(rate_drop_dense)(merged)
+#merged = BatchNormalization()(merged)
+#
+#preds = Dense(1, activation='sigmoid')(merged)
 
-        # Initialize all variables
-        sess.run(tf.global_variables_initializer())
+########################################
+## train the model
+########################################
+model = Model(inputs=[sequence_1_input, sequence_2_input], outputs=preds)      
+model.compile(loss='binary_crossentropy',
+        optimizer='Adam',
+        metrics=['accuracy'])
 
-        def train_step(x_batch_q1, x_batch_q2, y_batch):
-            """
-            A single training step
-            """
-            feed_dict = {
-              cnn.input_x: x_batch_q1,
-              cnn.input_x2: x_batch_q2,
-              cnn.input_y: y_batch,
-              cnn.dropout_keep_prob: FLAGS.dropout_keep_prob
-            }
-            _, step, summaries, loss, accuracy = sess.run(
-                [train_op, global_step, train_summary_op, cnn.loss, cnn.accuracy],
-                feed_dict)
-            time_str = datetime.datetime.now().isoformat()
-            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
-            train_summary_writer.add_summary(summaries, step)
-            
-            return loss, accuracy
+# Output directory for models and summaries
+timestamp = str(int(time.time()))
+out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
+os.mkdir(out_dir)
+print("Writing to {}\n".format(out_dir))
 
-        def dev_step(x_batch_q1, x_batch_q2, y_batch, writer=None):
-            """
-            Evaluates model on a dev set
-            """
-            feed_dict = {
-              cnn.input_x: x_batch_q1,
-              cnn.input_x2: x_batch_q2,
-              cnn.input_y: y_batch,
-              cnn.dropout_keep_prob: 1.0
-            }
-            step, summaries, loss, accuracy = sess.run(
-                [global_step, dev_summary_op, cnn.loss, cnn.accuracy],
-                feed_dict)
-            time_str = datetime.datetime.now().isoformat()
-            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
-            if writer:
-                writer.add_summary(summaries, step)
+early_stopping =EarlyStopping(monitor='val_loss', patience=3)
+bst_model_path = os.path.join(out_dir, 'model.h5')
+model_checkpoint = ModelCheckpoint(bst_model_path, save_best_only=True, save_weights_only=True)
 
-            
-        # Generate batches
-        batches = data_helpers.batch_iter(
-            list(zip(x_train_q1, x_train_q2, y_train)), FLAGS.batch_size, FLAGS.num_epochs)
-        
-        # Training loop. For each batch...
-        for batch in batches:
-            x_batch_q1, x_batch_q2, y_batch = zip(*batch)
-            loss, accuracy = train_step(x_batch_q1, x_batch_q2, y_batch)
-            current_step = tf.train.global_step(sess, global_step)
-            
-            if current_step % FLAGS.evaluate_every == 0:
-                print("\nEvaluation:")
-                dev_step(x_dev_q1, x_dev_q2, y_dev, writer=dev_summary_writer)
-                print("")
-            if current_step % FLAGS.checkpoint_every == 0:
-                path = saver.save(sess, checkpoint_prefix, global_step=current_step)
-                print("Saved model checkpoint to {}\n".format(path))
+hist = model.fit([data_1_train, data_2_train], labels_train, \
+        validation_data=([data_1_val, data_2_val], labels_val), \
+        epochs=FLAGS.num_epochs, batch_size=FLAGS.batch_size, shuffle=True, \
+        class_weight=class_weight, callbacks=[early_stopping, model_checkpoint])
+
+model.load_weights(bst_model_path)
+bst_val_score = min(hist.history['val_loss'])
+
+########################################
+## make the submission
+########################################
+print('Start making the submission before fine-tuning')
+
+preds = model.predict([test_data_1, test_data_2], batch_size=2048, verbose=1)
+preds += model.predict([test_data_2, test_data_1], batch_size=2048, verbose=1)
+preds /= 2
+
+submission = pd.DataFrame({'test_id':test_id, 'is_duplicate':preds.ravel()})
+bst_data_path = os.path.join(out_dir, 'sumbit.csv')
+submission.to_csv(bst_data_path, index=False)
+
+#%%
